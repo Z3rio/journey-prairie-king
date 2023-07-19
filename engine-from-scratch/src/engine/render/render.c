38af63c6f75e25963df01ba0b95568c033428bd3
Zerio <neoboij@gmail.com>
2023-07-19 02:41:06 +0200

    physics 👀 (kinda?)

--- engine-from-scratch/src/engine/render/render.c
+++ engine-from-scratch/src/engine/render/render.c
@@ -8,7 +8,7 @@ static Render_State_Internal state = {0};
 
 void render_init(void) {
   global.render.width = 800;
-  global.render.height = 600;
+  global.render.height = 800;
   global.render.window = render_init_window(global.render.width, global.render.height);
 
   render_init_shaders(&state);
