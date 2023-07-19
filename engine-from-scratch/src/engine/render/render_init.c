7d70e859d47ee297302af5e0e58cd25addab9cea
Zerio <neoboij@gmail.com>
2023-07-19 03:14:22 +0200

    fix forgotten A value in rgba

--- engine-from-scratch/src/engine/render/render_init.c
+++ engine-from-scratch/src/engine/render/render_init.c
@@ -60,7 +60,7 @@ void render_init_color_texture(u32 *texture) {
   glGenTextures(1, texture);
   glBindTexture(GL_TEXTURE_2D, *texture);
 
-  u8 solid_white[4] = {255, 255, 255};
+  u8 solid_white[4] = {255, 255, 255, 255};
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, solid_white);
 
   glBindTexture(GL_TEXTURE, 0);
